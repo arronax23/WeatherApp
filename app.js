3883a2173dbbdf794fcfcd23e420027a03642c24
Wiktor <wiktorzielak@gmail.com>
2019-07-22 12:55:59 +0200

    1. zmiana

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(position => {
-    console.log(position);
-    let longitude = position.coords.longitude;
-    let latitude = position.coords.latitude;
-    let app_id = "fd781890dff9958ecdd0051bfcfeefa6";
-    //longitude = Math.round(longitude);
-    //latitude = Math.round(latitude);
+setInterval(() => {
+  let data_torun = {};
+  let data_plock = {};
+  const temp_torun = document.querySelector(".temp-torun");
+  const temp_plock = document.querySelector(".temp-plock");
 
-    let api_data = {};
-    const temp_span = document.querySelector(".temp");
+  // Toruń
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?id=3083271&APPID=fd781890dff9958ecdd0051bfcfeefa6"
+  )
+    .then(resp => {
+      return resp.json();
+    })
+    .then(data => {
+      data_torun = data;
+    })
+    .then(() => {
+      temp_torun.textContent = Math.round(data_torun.main.temp - 272.15);
+      console.log("Toruń: ", Math.round(data_torun.main.temp - 272.15));
+    });
 
-    fetch(
-      `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=fd781890dff9958ecdd0051bfcfeefa6`
-    )
-      .then(resp => {
-        return resp.json();
-      })
-      .then(data => {
-        console.log(data);
-        api_data = data;
-      })
-      .then(() => {
-        temp_span.textContent = Math.round(api_data.main.temp - 272.15);
-      });
-  });
-}
+  // Płock
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?id=3088825&APPID=fd781890dff9958ecdd0051bfcfeefa6"
+  )
+    .then(resp => {
+      return resp.json();
+    })
+    .then(data => {
+      data_plock = data;
+    })
+    .then(() => {
+      temp_plock.textContent = Math.round(data_plock.main.temp - 272.15);
+      console.log("Płock: ", Math.round(data_plock.main.temp - 272.15));
+    });
+}, 10000);
